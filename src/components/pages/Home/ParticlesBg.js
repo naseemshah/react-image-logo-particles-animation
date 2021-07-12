03486027647f7490d4fcb87d9921984c28a94bdc
Naseem Shah <naseemshahvarikodan@gmail.com>
2021-07-12 14:14:19 +0530

    set up basic background

--- src/components/pages/Home/ParticlesBg.js
+++ src/components/pages/Home/ParticlesBg.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import Particles from 'react-particles-js';
 
 function ParticlesBg(props) {
     return (
-        <div>
-            particles bg
+        <div className="particles-container">
+            <Particles 
+                width="100vw"
+                height="100vh"
+                params={{
+                    "particles": {
+                        "number": {
+                            "value": 50
+                        },
+                        "size": {
+                            "value": 3
+                        },
+                        color: {
+                            "value": "#ff0000"
+                        }
+                    },
+                    "fullScreen": {
+                        "enable": true,
+                        "zIndex": 1
+                      },
+                    "interactivity": {
+                        "events": {
+                            "onhover": {
+                                "enable": true,
+                                "mode": "repulse"
+                            }
+                        }
+                    }
+                }}
+            />
         </div>
     );
 }
