fd47f8b4090bac3aeed4e099670266917eb77fab
Naseem Shah <naseemshahvarikodan@gmail.com>
2021-07-12 15:09:20 +0530

    basic logo setup

--- src/components/pages/Home/ParticlesBg.js
+++ src/components/pages/Home/ParticlesBg.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Particles from 'react-particles-js';
+import bgParticlesConfig from './particlesjs-config.json'
 
 function ParticlesBg(props) {
     return (
@@ -15,10 +16,11 @@ function ParticlesBg(props) {
                         "size": {
                             "value": 3
                         },
-                        color: {
+                        "color": {
                             "value": "#ff0000"
                         }
                     },
+                    
                     "fullScreen": {
                         "enable": true,
                         "zIndex": 1
@@ -33,6 +35,79 @@ function ParticlesBg(props) {
                     }
                 }}
             />
+            {/* <Particles 
+                params={{
+                    "fpsLimit": 60,
+                    "particles": {
+                        "collisions": {
+                            "enable": false
+                        },
+                        "number": {
+                            "value": 200,
+                            "density": {
+                                "enable": false,
+                                "value_area": 100
+                            }
+                        },
+                        "line_linked": {
+                            "enable": true,
+                            "distance": 30,
+                            "opacity": 0.4
+                        },
+                        "move": {
+                            "speed": 0.2,
+                            "bounce": true
+                        },
+                        "opacity": {
+                            "anim": {
+                                "enable": true,
+                                "opacity_min": 0.05,
+                                "speed": 1,
+                                "sync": false
+                            },
+                            "value": 0.4
+                        }
+                    },
+                    "polygon": {
+                        "enable": true,
+                        "scale": 0.5,
+                        "type": "inline",
+                        "move": {
+                            "radius": 10
+                        },
+                        "url": "/test.svg",
+                        "inline": {
+                            "arrangement": "equidistant"
+                        },
+                        "draw": {
+                            "enable": true,
+                            "stroke": {
+                                "color": "rgba(255, 255, 255, .2)"
+                            }
+                        }
+                    },
+                    "retina_detect": false,
+                    "interactivity": {
+                        "events": {
+                            "onhover": {
+                                "enable": true,
+                                "mode": "repulse"
+                            },
+                            "resize": true
+                        },
+                        "modes": {
+                            "repulse": {
+                                "distance": 50,
+                                "duration": 0.1,
+                                "factor": 10,
+                                "speed": 1,
+                                "maxSpeed": 2,
+                                "easing": "ease-out-quad"
+                            },
+                        }
+                    }
+                }}
+            /> */}
         </div>
     );
 }
